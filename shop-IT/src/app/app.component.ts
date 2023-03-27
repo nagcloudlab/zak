7c0de307151cd5a54df3ebac62d388afded62a2e
Nagabhushanam <nagabhushanam.n@revature.com>
2023-03-27 11:43:19 +0530

    Exercise-1

--- shop-IT/src/app/app.component.ts
+++ shop-IT/src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'shop-IT';
+
+  products: Array<any> = [
+    {
+      id: 1,
+      name: 'Laptop',
+      price: 150000,
+      description: 'Mac pro',
+      isAvailable: true,
+      imgPath: 'assets/Laptop.png'
+    },
+    {
+      id: 2,
+      name: 'Mobile',
+      price: 47000,
+      description: 'Iphone pro',
+      isAvailable: false,
+      imgPath: 'assets/Mobile.png'
+    },
+  ]
+
+  currentTab: number = 1
+
+  handleTabChange(event: Event, tabIndex: number) {
+    this.currentTab = tabIndex
+  }
+  isTabSelected(tabIndex: number) {
+    return this.currentTab === tabIndex
+  }
+
+
 }
